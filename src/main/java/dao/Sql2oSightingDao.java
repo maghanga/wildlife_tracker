ba0f4b82cc1b6d9425f5cd66624bee85e0285ad7
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-15 09:48:41 +0300

    Create Sql2oDao for Sighting class and add the necessary overriden methods

---
+++ src/main/java/dao/Sql2oSightingDao.java
@@ -0,0 +1,35 @@
+package dao;
+import models.Sighting;
+import org.sql2o.*;
+import java.util.List;
+
+
+
+public class Sql2oSightingDao implements SightingDao {
+
+    private final Sql2o sql2o;
+
+
+    public Sql2oSightingDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public List<Sighting> getAll() {
+        try(Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM sightings")
+                    .executeAndFetch(Sighting.class);
+        }
+    }
+
+    @Override
+    public void add(Sighting sighting) {
+        String sql = "INSERT INTO sightings (animal_id,sight_location,ranger_name) VALUES (:animal_id,:sight_location,:ranger_name)";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .bind(sighting)
+                    .executeUpdate();
+
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+}
