64bed274ed151d9facac15f8b6afd8c1b537f70c
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-15 09:29:00 +0300

    Create a Sql2oDao for EndangeredAnimal class and override the methods that will be included in the interface with necessary logic

---
+++ src/main/java/dao/Sql2oEndangeredAnimalDao.java
@@ -0,0 +1,31 @@
+package dao;
+
+import models.EndangeredAnimal;
+import org.sql2o.*;
+import java.util.List;
+
+public class Sql2oEndangeredAnimalDao implements EndangeredAnimalDao{
+
+    private final Sql2o sql2o;
+
+    public Sql2oEndangeredDao(Sql2o sql2o) { this.sql2o = sql2o; }
+
+    @Override
+    public List<EndangeredAnimal> getAll() {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM endangered_animals")
+                    .throwOnMappingFailure(false).executeAndFetch(EndangeredAnimal.class);
+        }
+    }
+
+    @Override
+    public EndangeredAnimal findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM endangered_animals WHERE id = :id")
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(EndangeredAnimal.class);
+        }
+
+    }
+}
