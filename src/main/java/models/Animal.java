f8652bd6daaab0b114af813008f8ee33bb14426f
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-15 07:31:58 +0300

    Add attributes, constructor, getter and setter methods in the Animal class

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,36 @@
 package models;
 
 public class Animal {
+    private int id;
+    private String animal_name;
+    private int sighting_id;
+
+    public Animal(int sighting_id, String animal_name){
+        this.sighting_id = sighting_id;
+        this.animal_name = animal_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAnimal_name() {
+        return animal_name;
+    }
+
+    public void setAnimal_name(String animal_name) {
+        this.animal_name = animal_name;
+    }
+
+    public int getSighting_id() {
+        return sighting_id;
+    }
+
+    public void setSighting_id(int sighting_id) {
+        this.sighting_id = sighting_id;
+    }
 }
