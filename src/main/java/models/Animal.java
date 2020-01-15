d4c971b44ae69e8d5eb411d353fce2e2fa6c12c9
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-15 08:50:12 +0300

    Endangered class inherits attributes from the Animal class correctly

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,27 +1,17 @@
 package models;
 
 public abstract class Animal {
-    private int id;
-    private String animal_name;
+    public int id;
+    public String animal_name;
 
-    public Animal(String animal_name){
-        this.animal_name = animal_name;
+    public Animal(){
     }
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getAnimal_name() {
         return animal_name;
     }
-
-    public void setAnimal_name(String animal_name) {
-        this.animal_name = animal_name;
-    }
-
 }
--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,40 +1,19 @@
 package models;
 
-public class EndangeredAnimal {
-    private int id;
-    private String animal_name;
+public class EndangeredAnimal extends Animal {
+
     private String animal_health;
     private String animal_age;
-    private int sighting_id;
 
-    public EndangeredAnimal(String animal_name, String animal_health, String animal_age){
+    public EndangeredAnimal(int id, String animal_name, String animal_age, String animal_health){
+        this.id = id;
         this.animal_name = animal_name;
         this.animal_health = animal_health;
         this.animal_age = animal_age;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getAnimal_name() {
-        return animal_name;
-    }
-
-    public void setAnimal_name(String animal_name) {
-        this.animal_name = animal_name;
-    }
-
     public String getAnimal_health() {
         return animal_health;
-    }
-
-    public void setAnimal_health(String animal_health) {
-        this.animal_health = animal_health;
     }
 
     public String getAnimal_age() {
@@ -43,13 +22,5 @@ public class EndangeredAnimal {
 
     public void setAnimal_age(String animal_age) {
         this.animal_age = animal_age;
-    }
-
-    public int getSighting_id() {
-        return sighting_id;
-    }
-
-    public void setSighting_id(int sighting_id) {
-        this.sighting_id = sighting_id;
     }
 }
